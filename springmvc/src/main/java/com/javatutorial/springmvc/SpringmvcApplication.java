ad90d12e00cb4e8655d91a199a7535e6e57f14c4
Jennie <ms.jl.cho@gmail.com>
2022-07-05 10:53:14 -0400

    First Commit

---
+++ springmvc/src/main/java/com/javatutorial/springmvc/SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.javatutorial.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcApplication.class, args);
+	}
+
+}
